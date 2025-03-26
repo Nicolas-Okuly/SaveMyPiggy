c77c71190417ef591779aea85cc720009fcf2563
Nicolas Okuly <nicolasokuly@outlook.com>
2025-03-25 22:41:02 -0700

    Complete the pin frontend

--- app/views/scripts/pin.js
+++ app/views/scripts/pin.js
@@ -2,11 +2,17 @@ let setPin;
 let getPin;
 let gotPin;
 
+const CheckableParams = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+});
+
+
 new QWebChannel(qt.webChannelTransport, (channel) => {
     setPin = channel.objects.setPin;
     getPin = channel.objects.getPin;
 
     getPin.sendPinData.connect(async (message) => {
+        if(message == 0) return handleNoPin();
         gotPin = message;
     });
 
@@ -18,6 +24,15 @@ new QWebChannel(qt.webChannelTransport, (channel) => {
     // setPin.receiveNewPin(1234);
 });
 
+function handleNoPin() {
+    if (CheckableParams.newpin) {
+        setPin.receiveNewPin(CheckableParams.newpin);
+        window.location = window.location;
+        return;
+    }
+    window.location = window.location.host + "nopin.html";
+}
+
 /**
  * Handle the pin data and process it all
  * @param {Number} pin 
@@ -25,16 +40,13 @@ new QWebChannel(qt.webChannelTransport, (channel) => {
 function handlePinData(pin) {
     if (pin == gotPin) window.location = "index.html";
     else {
+        document.getElementById("error").innerText = "The PIN was incorrect. Please try again."; // Update error message
         if(window.location.href.includes("?failed=true")) 
             window.location = window.location;
-        else 
+        else
             window.location = `${window.location}?failed=true`;
     }
 }
-
-const CheckableParams = new Proxy(new URLSearchParams(window.location.search), {
-    get: (searchParams, prop) => searchParams.get(prop),
-});
 
 if (CheckableParams.failed == "true") {
     document.getElementById("error").innerText = "Please try again"
